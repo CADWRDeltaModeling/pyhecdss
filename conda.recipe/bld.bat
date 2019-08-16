55eea3c2d00898b8203438525c142791a635d516
Nicky Sandhu <psandhu@water.ca.gov>
2019-08-16 08:25:08 -0700

    added recipe for conda build and packaging

---
+++ conda.recipe/bld.bat
@@ -0,0 +1,3 @@
+set PATH=d:\Programs\swigwin-4.0.0;%PATH%
+python setup.py install
+if errorlevel 1 exit 1
--- setup.py
+++ setup.py
@@ -7,30 +7,7 @@ from distutils import sysconfig
 
 # Third-party modules - we depend on numpy for everything
 import re
-import requests
 import numpy
-
-
-def check_numpy_i():
-    np_version = re.compile(r'(?P<MAJOR>[0-9]+)\.'
-                            '(?P<MINOR>[0-9]+)') \
-        .search(numpy.__version__)
-    np_version_string = np_version.group()
-    np_version_info = {key: int(value)
-                       for key, value in np_version.groupdict().items()}
-
-    np_file_name = 'numpy.i'
-    np_file_url = 'https://raw.githubusercontent.com/numpy/numpy/maintenance/' + \
-                  np_version_string + '.x/tools/swig/' + np_file_name
-    if(np_version_info['MAJOR'] == 1 and np_version_info['MINOR'] < 9):
-        np_file_url = np_file_url.replace('tools', 'doc')
-
-    chunk_size = 8196
-    with open(np_file_name, 'wb') as file:
-        for chunk in requests.get(np_file_url,
-                                  stream=True).iter_content(chunk_size):
-            file.write(chunk)
-
 
 def get_numpy_include():
     # Obtain the numpy include directory.  This logic works across numpy versions.
