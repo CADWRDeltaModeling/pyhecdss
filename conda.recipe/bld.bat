5da527b986cb4273ba91a7e8ed50ee0d328a6d80
Kijin Nam <knam@water.ca.gov>
2019-10-15 11:40:50 -0700

    Add a Windows CI batch file

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,3 +1,4 @@
-set PATH=d:\Programs\swigwin-4.0.0;%PATH%
-python setup.py install
+REM set PATH=d:\Programs\swigwin-4.0.0;%PATH%
+python setup.py build_ext --inplace
+python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
